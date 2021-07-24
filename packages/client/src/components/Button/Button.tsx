74854f46d93345a062e8a0f5973d2f6bad823c9f
Brian Clovis <bwclovis.web@gmail.com>
2021-07-24 06:36:56 -0400

    add interface

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -1,6 +1,8 @@
+import { FC } from "react"
 import ButtonStyles from "./ButtonStyles"
+import { ButtonI } from './Interfaces'
 
-const Button = ({ children, config, onClick }) => {
+const Button: FC<ButtonI> = ({ children, config, onClick }) => {
     return (
         <ButtonStyles config={config} onClick={onClick}>
             {children}
--- packages/client/src/components/Button/Interfaces.tsx
+++ packages/client/src/components/Button/Interfaces.tsx
@@ -1,3 +1,11 @@
+import { ReactNode } from "react";
+
 export interface ButtonStylesI {
     config: 'primary' | 'secondary' | 'ghost'
+}
+
+export interface ButtonI {
+    config: 'primary' | 'secondary' | 'ghost'
+    children: ReactNode
+    onClick: () => void
 }
